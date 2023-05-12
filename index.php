c45f9f665ca66d8218925c5288dfe46f8d30e1e2
Noureddine145 <133378203+Noureddine145@users.noreply.github.com>
2023-05-12 16:48:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welkom op deze pagina</h1>
+    <?php echo "hello world"; ?>
+
+    <br>
+    <?php
+    $hello = 10;
+    $doei = 12;
+    echo $hello+$doei;
+    ?>
+
+    <br>
+    <?php
+    $hello = 10;
+    $doei = 12;
+    echo 10*12;
+    ?>
+
+    <br>
+    <?php
+    $naam="Noureddine";
+    echo "welkom"  <div class="1Noureddine">
+    ?>
+
+    <br>
+    <?php
+
+    $mijnNaam="Marwan";
+    $jouwNaam="mo";
+    echo $mijnNaam. "zit bij" . $jouwNaam . "in de klas!"
+
+    ?>
+    
+</body>
+</html>
